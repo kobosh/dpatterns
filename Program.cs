9b2923359713cdf36b5792006af3c0d8b8fffd9b
kobosh <kokobosh@gmail.com>
2020-03-15 12:16:51 -0400

    commit add my prject to github

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FactorypatternLib;
+using System.Reflection;
+namespace FactoryPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           /* Assembly assemblyx = Assembly.Load("FactorypatternLib");
+            foreach (var assemblyName in Assembly.GetExecutingAssembly()
+                .GetReferencedAssemblies())
+            {
+                Assembly assembly = Assembly.Load(assemblyName);
+                foreach (var type in assembly.GetTypes())
+                {
+                    Console.WriteLine(type.Name);
+                }
+            }*/
+
+            //Assembly.GetAssembly()
+            MachineFactory mf = new MachineFactory();
+         var r=   mf.CreateInstance("rob");
+            r.TurnOn();
+
+            //Type[] types = Assembly.GetAssembly( typeof(FactorypatternLib.)).GetTypes();
+            Console.WriteLine();
+        }
+    }
+}
